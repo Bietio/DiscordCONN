f1a843733831e97582be2445f0e007dad953d19e
bietio <consoler123456789@gmail.com>
2025-04-14 20:34:13 -0300

    First commit

---
+++ css.php
@@ -0,0 +1,14 @@
+<style>
+    * {
+        background-color: rgb(31, 31, 40);
+        color: white;
+        text-align: center;
+        font-weight: 1000;
+        font-size: larger;
+    }
+
+    h1 {
+        font-weight: 500;
+        color: #504070;
+    }
+</style>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'css.php';
+
+echo "<h1>DiscordCONN_v1.0</h1>";
+
+//Iniciar
+$ch = curl_init('https://discord.com/api/v9/channels/1322434807464198205/messages');
+
+$config = json_decode(file_get_contents('config.json'));
+
+
+//Configução;
+curl_setopt_array($ch,[
+    CURLOPT_POST           => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER     => str_replace('{TOKEN}', $config->token, $config->headers),
+    CURLOPT_POSTFIELDS     => json_encode($config->data)
+]);
+
+//Executar
+$response = curl_exec($ch);
+
+//Erros, Output
+if (curl_errno($ch)) 
+{
+    echo 'Error: ' . curl_error($ch);
+} else {
+    echo 'Response: ' . $response;
+}
+
+//Encerrar
+curl_close($ch);
+?>
